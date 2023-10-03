6ca8d39c43a8f6dbd461bc1ad014ef1ed0a8cbea
salehrashid <saleh.rashid.b@gmail.com>
2023-10-03 18:53:46 +0700

    apply major upgrade depedency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/salehrashid/go-modules-app
 
 go 1.21
 
-require github.com/salehrashid/go-modules v1.0.1
+require github.com/salehrashid/go-modules/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import go_modules "github.com/salehrashid/go-modules"
+import go_modules "github.com/salehrashid/go-modules/v2"
 
 func main() {
-	println(go_modules.Core())
+	println(go_modules.Core(" saleh"))
 }
